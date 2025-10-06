d28e2d4a06633c7f26d62572165b5547059e9882
Alejandro Teran <terancruzalejandro@gmail.com>
2025-10-06 11:12:27 -0500

    laboratorio3 completado

---
+++ laboratorio3/laboratorio31/Program.cs
@@ -0,0 +1,37 @@
+﻿//1. El usuario debe ingresar dos números y la aplicación consola mostrará el resultado de la 
+//operación(a + b) * (a - b).La operación debe estar bajo el método Calcular de la clase 
+//CalculosMatematicos y el programa principal bajo la clase Program 
+
+using System;
+
+public class CalculosMatematicos
+{
+    //Metodo que realiza la operacio
+    public double Calcular(double a, double b)
+    {
+        double resultado = (a + b) * (a - b);
+        return resultado;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CalculosMatematicos calc = new CalculosMatematicos();
+
+        //se crea un objeto de la clase CalculosMatematicos
+        Console.WriteLine("Ingrese el primer numero (a):");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo numero (b):");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        //se llama el metodo calcular y se guarda el resultado
+
+        double resultado = calc.Calcular(a, b);
+
+        Console.WriteLine("El resultado de (a+b) * (a-b) es:" + resultado);
+
+    }
+}
---
+++ laboratorio3/laboratorio33/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class CalculosMatematicos
+{
+    // Método que realiza la operación (a + b) * (a - b)
+    public double Calcular(double a, double b)
+    {
+        double resultado = (a + b) * (a - b);
+        return resultado;
+    }
+
+    // Método que calcula el área de un círculo
+    public double CalculoArea(double radio)
+    {
+        double area = Math.PI * Math.Pow(radio, 2);
+        return area;
+    }
+
+    // calcula el perímetro de un rectángulo
+    public double CalcularPerimetroRectangulo(double lado1, double lado2)
+    {
+        double perimetro = 2 * (lado1 + lado2);
+        return perimetro;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Crear un objeto de la clase CalculosMatematicos
+        CalculosMatematicos calc = new CalculosMatematicos();
+
+        // Pedir los lados del rectángulo
+        Console.WriteLine("Ingrese la longitud del primer lado del rectángulo:");
+        double lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la longitud del segundo lado del rectángulo:");
+        double lado2 = Convert.ToDouble(Console.ReadLine());
+
+        // Llamar al método que calcula el perímetro
+        double perimetro = calc.CalcularPerimetroRectangulo(lado1, lado2);
+
+        // Mostrar el resultado
+        Console.WriteLine("El perímetro del rectángulo es: " + perimetro);
+    }
+}
