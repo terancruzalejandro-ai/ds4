3256100519763f54d6d6304fcc2e3dc1a1e525c4
Alejandro Teran <terancruzalejandro@gmail.com>
2025-10-15 21:12:31 -0500

    laboratorio 11 completado

---
+++ laboratorio4/laboratorio41/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n, x;
+        string linea;
+        Console.Write("Ingrese el valor de n: ");
+        linea = Console.ReadLine();
+        n = int.Parse(linea);
+        x = 1;
+
+        while (x <= n)
+        {
+            Console.Write(x);
+            Console.Write(" , ");
+            x = x + 1;
+        }
+        Console.ReadKey();
+    }
+}
