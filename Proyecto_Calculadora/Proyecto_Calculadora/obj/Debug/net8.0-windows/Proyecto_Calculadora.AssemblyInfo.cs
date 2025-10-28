1567700cdfbf505ca5b3409f5f8f64e111ebe561
Alejandro Teran <terancruzalejandro@gmail.com>
2025-10-27 23:43:30 -0500

    Actualiza versión del ensamblado y rutas de usuario
    Se actualizó la versión informativa del ensamblado en `Proyecto_Calculadora.AssemblyInfo.cs`.
    Se modificaron las rutas de usuario en varios archivos de configuración, cambiando "migmartinez" por "Ricky".
    Esto incluye cambios en `Proyecto_Calculadora.designer.runtimeconfig.json`, `Proyecto_Calculadora.csproj.nuget.dgspec.json`, `Proyecto_Calculadora.csproj.nuget.g.props`, `project.assets.json`, y `project.nuget.cache`.
    Además, se registraron cambios binarios en `Proyecto_Calculadora.AssemblyInfoInputs.cache` y `Proyecto_Calculadora.assets.cache`, y se actualizó el hash de especificación del proyecto en `project.nuget.cache`.

--- Proyecto_Calculadora/Proyecto_Calculadora/obj/Debug/net8.0-windows/Proyecto_Calculadora.AssemblyInfo.cs
+++ Proyecto_Calculadora/Proyecto_Calculadora/obj/Debug/net8.0-windows/Proyecto_Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto_Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9bebedd4aa4a5ad684e9b5773a3e58a8f40b6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487453cc587be5bf815603639c0f5371dfdb2050")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto_Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto_Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
